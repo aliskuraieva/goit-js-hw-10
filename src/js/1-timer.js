8662860cccbfcafaed52b6426be57fd0d6196c68
Alisa Kuraieva <alisa.mogila@gmail.com>
2024-08-13 22:20:14 +0300

    fix

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -4,15 +4,15 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const inputField = document.querySelector('#datetime-picker');
-const startBtn = document.querySelector('button[data-start]');
-const daysSpan = document.querySelector('span[data-days]');
-const hoursSpan = document.querySelector('span[data-hours]');
-const minutesSpan = document.querySelector('span[data-minutes]');
-const secondsSpan = document.querySelector('span[data-seconds]');
+const startBtn = document.querySelector('[data-start]');
+const daysSpan = document.querySelector('[data-days]');
+const hoursSpan = document.querySelector('[data-hours]');
+const minutesSpan = document.querySelector('[data-minutes]');
+const secondsSpan = document.querySelector('[data-seconds]');
 
 startBtn.addEventListener('click', startTimer);
 
-let userSelectedDate;
+let userSelectedDate = null;
 let timer = null;
 
 const options = {
@@ -21,64 +21,43 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    if (options.defaultDate >= selectedDates[0]) {
-      startBtn.disabled = true;
-      // підключаємо повідомлення невдалого вибору
-      iziToast.error({
-        title: 'Error',
+    if (selectedDates[0] <= new Date()) {
+      return iziToast.error({
+        position: 'topRight',
+
         message: 'Please choose a date in the future',
       });
-    } else {
-      startBtn.disabled = false; // Дозволяємо натискати кнопку Start, якщо вибрано правильну дату
-      userSelectedDate = selectedDates[0];
-      // підключаємо повідомлення вдалого вибору
-      iziToast.success({
-        title: 'OK',
-        message: 'You can press "Start"',
-      });
     }
+    userSelectedDate = selectedDates[0];
+    startBtn.disabled = false;
   },
 };
 
 flatpickr(inputField, options);
 
-// форматуємо рядок; додаємо два нулі на початок рядка, що відображалось так 00Х
-const addLeadingZero = value => value.toString().padStart(2, '0');
-
-startBtn.addEventListener('click', startTimer);
-
 function startTimer() {
-  startBtn.disabled = true;
   inputField.disabled = true;
+  startBtn.disabled = true;
 
-  timer = setInterval(() => {
-    const currentDate = new Date();
-    const timeDiff = userSelectedDate - currentDate;
+  timer = setInterval(updateTimer, 1000);
+  return;
+}
 
-    const { days, hours, minutes, seconds } = convertMs(timeDiff);
+function updateTimer() {
+  const timeDiff = userSelectedDate - new Date();
 
-    daysSpan.textContent = addLeadingZero(days);
-    hoursSpan.textContent = addLeadingZero(hours);
-    minutesSpan.textContent = addLeadingZero(minutes);
-    secondsSpan.textContent = addLeadingZero(seconds);
+  if (timeDiff <= 0) {
+    inputField.disabled = false;
+    clearInterval(timer);
+    return;
+  }
 
-    if (timeDiff <= 0) {
-      clearInterval(timer);
-      inputField.disabled = false;
+  const { days, hours, minutes, seconds } = convertMs(timeDiff);
 
-      daysSpan.textContent = '00';
-      hoursSpan.textContent = '00';
-      minutesSpan.textContent = '00';
-      secondsSpan.textContent = '00';
-
-      iziToast.info({
-        title: 'Finished',
-        message: 'The countdown is over!',
-      });
-      return;
-    }
-  }, 1000);
+  daysSpan.textContent = String(days).padStart(2, 0);
+  hoursSpan.textContent = String(hours).padStart(2, 0);
+  minutesSpan.textContent = String(minutes).padStart(2, 0);
+  secondsSpan.textContent = String(seconds).padStart(2, 0);
 }
 
 function convertMs(ms) {
@@ -100,17 +79,16 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-
 // const inputField = document.querySelector('#datetime-picker');
-// const startBtn = document.querySelector('[data-start]');
-// const daysSpan = document.querySelector('[data-days]');
-// const hoursSpan = document.querySelector('[data-hours]');
-// const minutesSpan = document.querySelector('[data-minutes]');
-// const secondsSpan = document.querySelector('[data-seconds]');
+// const startBtn = document.querySelector('button[data-start]');
+// const daysSpan = document.querySelector('span[data-days]');
+// const hoursSpan = document.querySelector('span[data-hours]');
+// const minutesSpan = document.querySelector('span[data-minutes]');
+// const secondsSpan = document.querySelector('span[data-seconds]');
 
 // startBtn.addEventListener('click', startTimer);
 
-// let userSelectedDate;
+// let userSelectedDate = null;
 // let timer = null;
 
 // const options = {
@@ -119,43 +97,59 @@ function convertMs(ms) {
 //   defaultDate: new Date(),
 //   minuteIncrement: 1,
 //   onClose(selectedDates) {
-//     if (selectedDates[0] <= new Date()) {
-//       return iziToast.error({
-//         position: 'topRight',
-
+//     console.log(selectedDates[0]);
+//     if (options.defaultDate >= selectedDates[0]) {
+//       startBtn.disabled = true;
+//       // підключаємо повідомлення невдалого вибору
+//       iziToast.error({
+//         title: 'Error',
 //         message: 'Please choose a date in the future',
 //       });
+//     } else {
+//       startBtn.disabled = false; // Дозволяємо натискати кнопку Start, якщо вибрано правильну дату
+//       userSelectedDate = selectedDates[0];
+//       // підключаємо повідомлення вдалого вибору
+//       iziToast.success({
+//         title: 'OK',
+//         message: 'You can press "Start"',
+//       });
 //     }
-//     userSelectedDate = selectedDates[0];
-//     startBtn.disabled = false;
 //   },
 // };
 
 // flatpickr(inputField, options);
 
+// // форматуємо рядок; додаємо два нулі на початок рядка, що відображалось так 00Х
+// const addLeadingZero = value => value.toString().padStart(2, '0');
+
+// startBtn.addEventListener('click', startTimer);
+
 // function startTimer() {
-//   inputField.disabled = true;
 //   startBtn.disabled = true;
+//   inputField.disabled = true;
 
-//   timer = setInterval(updateTimer, 1000);
-//   return;
-// }
+//   timer = setInterval(() => {
+//     const currentDate = new Date();
+//     const timeDiff = userSelectedDate - currentDate;
 
-// function updateTimer() {
-//   const timeDiff = userSelectedDate - new Date();
+//     const { days, hours, minutes, seconds } = convertMs(timeDiff);
 
-//   if (timeDiff <= 0) {
-//     inputField.disabled = false;
-//     clearInterval(timer);
-//     return;
-//   }
+//     daysSpan.textContent = addLeadingZero(days);
+//     hoursSpan.textContent = addLeadingZero(hours);
+//     minutesSpan.textContent = addLeadingZero(minutes);
+//     secondsSpan.textContent = addLeadingZero(seconds);
 
-//   const { days, hours, minutes, seconds } = convertMs(timeDiff);
+//     if (timeDiff <= 0) {
+//       clearInterval(timer);
+//       inputField.disabled = false;
 
-//   daysSpan.textContent = String(days).padStart(2, 0);
-//   hoursSpan.textContent = String(hours).padStart(2, 0);
-//   minutesSpan.textContent = String(minutes).padStart(2, 0);
-//   secondsSpan.textContent = String(seconds).padStart(2, 0);
+//       daysSpan.textContent = '00';
+//       hoursSpan.textContent = '00';
+//       minutesSpan.textContent = '00';
+//       secondsSpan.textContent = '00';
+//       return;
+//     }
+//   }, 1000);
 // }
 
 // function convertMs(ms) {
@@ -176,7 +170,6 @@ function convertMs(ms) {
 
 //   return { days, hours, minutes, seconds };
 // }
-
 
 console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
