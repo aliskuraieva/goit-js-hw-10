24a3a55478ae586a81744ba5aeb895cd1e6e4592
Alisa Kuraieva <alisa.mogila@gmail.com>
2024-08-13 21:14:43 +0300

    Update 2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,35 +1,36 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const formEl = document.querySelector('.form');
-formEl.addEventListener('submit', event => {
+const form = document.querySelector('.form');
+form.addEventListener('submit', formSubmit);
+function formSubmit(event) {
   event.preventDefault();
-  const delay = Number(formEl.elements.delay.value);
-  const state = formEl.elements.state.value;
-  createNotification(delay, state);
-});
-function createNotification(delay, state) {
-  const delayPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (state === 'fulfilled') {
-        resolve(delay);
-      } else {
-        reject(delay);
-      }
-    }, delay);
-  });
-  delayPromise
+
+  const { delay, state } = event.currentTarget.elements;
+  createPromise(delay.value, state.value)
     .then(delay => {
       iziToast.success({
         title: 'OK',
-        message: `✅ Fulfilled promise in ${delay} ms!`,
+        message: `✅ Fulfilled promise in ${delay}ms`,
       });
     })
     .catch(delay => {
       iziToast.error({
         title: 'Error',
-        message: `❌ Rejected promise in ${delay} ms`,
+        message: `❌ Rejected promise in ${delay}ms`,
       });
     });
-  form.reset();
+  event.currentTarget.reset();
+}
+
+function createPromise(currentDelay, currentState) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (currentState === 'fulfilled') {
+        resolve(currentDelay);
+      } else {
+        reject(currentDelay);
+      }
+    }, currentDelay);
+  });
 }
